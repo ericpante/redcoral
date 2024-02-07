f856997ba695d4d35694740fd51896763fca49d1
Didier AURELLE <44087665+didier-aurelle@users.noreply.github.com>
2024-02-07 20:39:41 +0100

    Update vcf_analyses.sh

--- vcf_analyses.sh
+++ vcf_analyses.sh
@@ -1,3 +1,6 @@
 # analyses of vcf files
 # to be launched directly on local computer or integrated in a script for submission to computing cluster
 
+# you will need vcftools: https://vcftools.github.io/ 
+
+# go to the directory
