b9b058c5344c093957cd4cd46c8df0225c6f7bb6
Eric <pante.eric@gmail.com>
2024-02-12 15:23:50 +0100

    Delete vcf_analyses_old_to_delete.sh

--- vcf_analyses_old_to_delete.sh
+++
@@ -1,6 +0,0 @@
-# analyses of vcf files
-# to be launched directly on local computer or integrated in a script for submission to computing cluster
-
-# you will need vcftools: https://vcftools.github.io/ 
-
-# go to the directory
